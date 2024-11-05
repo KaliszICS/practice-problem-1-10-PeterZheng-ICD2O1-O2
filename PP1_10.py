924e443b1f167670e4a08dbe887dd06dd7b0798d
PeterZheng-ICD2O1-O2 <Peter.zheng@student.tdsb.on.ca>
2024-11-05 22:26:26 +0000

    a

--- PP1_10.py
+++ PP1_10.py
@@ -2,25 +2,38 @@
 
 def q1(): 
   #Write Assignment code here
-
-
+  import math
+  num = input("Input a number: ")
+  int(num)
+  print (math.sqrt(int(num)))
+  
 def q2(): 
   #Write Assignment code here
-
-
+  import math
+  num = input ("Input a number: ")
+  print (math.isqrt(int(num)))
 def q3(): 
   #Write Assignment code here
-
+  import math
+  num = input ("Input a number: ")
+  print (math.floor(float(num)))
 
 def q4(): 
   #Write Assignment code here
-
+  import math
+  num = input ("Input a number: ")
+  print (math.ceil(float(num)))
 
 def q5(): 
   #Write Assignment code here
+  import math
+  num = input ("Input a number: ")
+  num2 = input ("Input another number: ")
+  answ = (int(num))* (int(num2))
 
-
-
+  int(answ)
+  answ2 = int(answ/2)
+  print (math.floor(answ2))
 #Do not alter the following code
 #Comment out the following code when running your tests
 
